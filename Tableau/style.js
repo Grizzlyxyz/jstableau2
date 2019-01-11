311de8aeaa9d6048fb607093af012518a022a99e
Grizzly <grizzlyxyz@gmail>
2019-01-11 14:22:38 +0100

    tableau exo

---
+++ Tableau/style.js
@@ -0,0 +1,50 @@
+//EXO1
+
+let tableauExo1 = ['Nicolas', 'Yassine', 'salim','sonia'];
+console.log(tableauExo1);
+console.log(tableauExo1(3));
+console.log(tableauExo1,lenght);
+
+//EXO2
+
+let tableauExo1 = ["nicolas"];
+console.log (tableauExo2);
+console.log(tableauExo2.push('Matine'));
+console.log(tableauExo2);
+
+//EXO3
+
+let tableauExo3 = ["Nicolas"];
+console.log(tableauExo3);
+tableauExo3.push("Jenni lopez","Beyonce");
+console.log(tableauExo3);
+tableauExo3.pop();
+console.log(tableauExo3);
+
+//EXO4
+let tableauExo4 = ["Nathan","Salim","Mourad","Salma","Sonia","Sonia"];
+tableauExo4.splice(2,1);
+console.log(tableauExo4);
+
+//EXO4B
+let tableauExo4B = ["Ismael","Youssef","Jonathan","Joaquim","Vadim"];
+tableauExo1.splice(1,1,'Abdellah');
+console.log(tableauExo4B);
+
+//EXO5 
+
+let classGroupeB =[];
+classGroupeB.unshift('Nisrine');
+classGroupeB.push('Ismael','Mohamed','Benjamin','Salma',);
+classGroupeB.push('Camelia');
+classGroupeB.shift();
+classGroupeB.pop();
+classGroupeB.push("Sonia","Abdelah","Louis","Mourad");
+classGroupeB.push("Nathan");
+classGroupeB.push("Salim","Youssef","Bilal");
+clas
+
+
+
+
+
